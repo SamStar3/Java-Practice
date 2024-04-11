ddc872bc17910295b4f44dab4bc58076d689b625
SamStar3 <samstar1044948@gmail.com>
2024-04-11 20:42:27 +0530

    "file added"

---
+++ src/ReadingInput/Main.java
@@ -0,0 +1,14 @@
+package ReadingInput;
+
+public class Main {
+    public static void main(String[] args){
+        int currentyear = 2024;
+        String dateOfBirth = "2001";
+        int age = Integer.parseInt(dateOfBirth);
+        System.out.println("Age " + (currentyear-age));
+        String userAgeWithPartialYear = "22.5";
+        double ageWithPartialYear = Double.parseDouble(userAgeWithPartialYear);
+        System.out.println( "The user say's heageWithPartialYear);
+
+    }
+}
