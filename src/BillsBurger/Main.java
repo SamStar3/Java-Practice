3f730febd1be007455ca5060077df48e6b806142
SamStar3 <samstar1044948@gmail.com>
2024-05-30 17:02:17 +0530

    file added

--- src/BillsBurger/Main.java
+++ src/BillsBurger/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Item cook = new Item("drink","coke",1.50);
-        cook.printItem();
-        cook.setSize("LARGE");
-        cook.printItem();
+//        Item cook = new Item("drink","coke",1.50);
+//        cook.printItem();
+//        cook.setSize("LARGE");
+//        cook.printItem();
+//
+//        Item avocado = new Item("Topping","avocado",1.50);
+//        avocado.printItem();
 
-        Item avocado = new Item("Topping","avocado",1.50);
-        avocado.printItem();
+        Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "CHEESE","MAYO");
+        burger.printItem();
     }
 }
---
+++ src/BillsBurger/MealOrder.java
@@ -0,0 +1,4 @@
+package BillsBurger;
+
+public class MealOrder {
+}
