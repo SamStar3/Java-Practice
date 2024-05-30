a9d78c02179e5cb3c3ff077f145a641a8f3cd5a6
SamStar3 <samstar1044948@gmail.com>
2024-05-30 15:20:35 +0530

    file added

--- src/BillsBurger/Main.java
+++ src/BillsBurger/Main.java
@@ -1,2 +1,15 @@
-package BillsBurger;public class Main {
+package BillsBurger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item cook = new Item("drink","coke",1.50);
+        cook.printItem();
+        cook.setSize("LARGE");
+        cook.printItem();
+
+        Item avocado = new Item("Topping","avocado",1.50);
+        avocado.printItem();
+    }
 }
