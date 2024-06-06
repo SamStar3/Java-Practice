8f3f9000e02fd8525796c1b0c00e0f48f4ce1aed
SamStar3 <samstar1044948@gmail.com>
2024-06-06 17:12:50 +0530

    updated

--- src/SortingDSA/CountInversions.java
+++ src/SortingDSA/CountInversions.java
@@ -1,4 +1,20 @@
 package SortingDSA;
 
 public class CountInversions {
+    public static void main (String[] args){
+        int[] arr = {5,4,2,1};
+
+        int res = 0;
+//        int j = 0;
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++) {
+            for (int j = i+1;j < n; j++) {
+                if(arr[i] > arr[j]) {
+                    res++;
+                }
+            }
+        }
+        System.out.println(res);
+    }
 }
