59962344a1aa5516cab2b58be28b0da310eeda9b
SamStar3 <samstar1044948@gmail.com>
2024-06-10 17:03:21 +0530

    file added

--- src/SortingDSA/CycleSort.java
+++ src/SortingDSA/CycleSort.java
@@ -1,2 +1,123 @@
-package SortingDSA;public class CycleSort {
+package SortingDSA;
+
+import java.util.Arrays;
+
+public class CycleSort {
+
+//    public static void main(String[] args) {
+//
+//        int[] arr = {20,30,10,50,40};
+//        int n = arr.length;
+//
+//        for (int cs = 0; cs < n - 1; cs++){
+//            int item = arr[cs];
+//            int pos = cs;
+//
+//            for (int i = cs + 1;i < n;i++){
+//                if (arr[i] < item){
+//                    pos++;
+//                }
+//                if (pos == cs) {
+//                    continue;
+//                }
+//                while (item == arr[pos]) {
+//                    pos += 1;
+//                }
+//                if (pos != cs) {
+//                    int temp = arr[i];
+//                    arr[i] = item;
+//                    item = temp;
+//                }
+//
+//                while (pos != cs) {
+//                    pos = cs;
+//                    for (int j = cs + 1;j < n; j++) {
+//                        if (arr[j] < item) {
+//                            pos++;
+//                        }
+//                        while (item == arr[pos]) {
+//                            pos += 1;
+//                        }
+//                        if (item != arr[pos]) {
+//                            int temp1 = arr[j];
+//                            arr[j] = item;
+//                            item = temp1;
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+//    }
+
+    public static void cycleSort(int arr[], int n)
+    {
+        // count number of memory writes
+        int writes = 0;
+
+        // traverse array elements and put it to on
+        // the right place
+        for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
+            // initialize item as starting point
+            int item = arr[cycle_start];
+
+            // Find position where we put the item. We basically
+            // count all smaller elements on right side of item.
+            int pos = cycle_start;
+            for (int i = cycle_start + 1; i < n; i++)
+                if (arr[i] < item)
+                    pos++;
+
+            // If item is already in correct position
+            if (pos == cycle_start)
+                continue;
+
+            // ignore all duplicate elements
+            while (item == arr[pos])
+                pos += 1;
+
+            // put the item to it's right position
+            if (pos != cycle_start) {
+                int temp = item;
+                item = arr[pos];
+                arr[pos] = temp;
+                writes++;
+            }
+
+            // Rotate rest of the cycle
+            while (pos != cycle_start) {
+                pos = cycle_start;
+
+                // Find position where we put the element
+                for (int i = cycle_start + 1; i < n; i++)
+                    if (arr[i] < item)
+                        pos += 1;
+
+                // ignore all duplicate elements
+                while (item == arr[pos])
+                    pos += 1;
+
+                // put the item to it's right position
+                if (item != arr[pos]) {
+                    int temp = item;
+                    item = arr[pos];
+                    arr[pos] = temp;
+                    writes++;
+                }
+            }
+        }
+    }
+
+    // Driver program to test above function
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 8, 3, 9, 10, 10, 2, 4 };
+        int n = arr.length;
+        cycleSort(arr, n);
+
+        System.out.println("After sort : ");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
 }
--- src/SortingDSA/HeapSort.java
+++ src/SortingDSA/HeapSort.java
@@ -1,2 +1,63 @@
-package SortingDSA;public class HeapSort {
+package SortingDSA;
+
+
+import javax.swing.plaf.PanelUI;
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2*i + 1; // left = 2*i + 1
+        int r = 2*i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest]= temp;
+//            swap(arr[i], arr[largest]);
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Main function for heap sort
+    public static void heapSort(int arr[], int n)
+    {
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+//            swap(arr[0], arr[i]);
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 10, 3, 5, 1};
+        int n = arr.length;
+        heapSort(arr,n);
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
