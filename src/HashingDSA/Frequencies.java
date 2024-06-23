8b439d15582bb213e220f46ca8f390ea9d9e1dbb
SamStar3 <samstar1044948@gmail.com>
2024-06-23 18:44:35 +0530

    file added

--- src/HashingDSA/Frequencies.java
+++ src/HashingDSA/Frequencies.java
@@ -1,2 +1,49 @@
-package HashingDSA;public class Frequencies {
+package HashingDSA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Frequencies {
+
+    static void frequencies(int[] arr) {
+
+        int n = arr.length;
+//        for (int i = 0; i < n; i++) {
+//            boolean flag = false;
+//            for (int j = 0; j < i; j++ ) {
+//                if (arr[i] == arr[j]) {
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//            if (flag == true) {
+//                continue;
+//            }
+//            int frq = 1;
+//            for (int k = i +1; k < n; k++) {
+//                if (arr[i] == arr[k]) {
+//                    frq++;
+//                }
+//            }
+//            System.out.println(frq);
+//        }
+
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        for (int x : arr){
+            h.put(x,h.getOrDefault(x,0) + 1 );
+        }
+        for (Map.Entry<Integer, Integer> e : h.entrySet()){
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+        System.out.println(h);
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {10,12,10,15,10,20,12,12};
+
+
+        frequencies(arr);
+    }
 }
--- src/HashingDSA/SortElement.java
+++ src/HashingDSA/SortElement.java
@@ -1,2 +1,47 @@
-package HashingDSA;public class SortElement {
+package HashingDSA;
+
+import java.util.*;
+
+public class SortElement {
+
+    static ArrayList<Integer> sortByFreq(int arr[], int n)
+    {
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> elements = new ArrayList<>();
+        for (int num : arr) {
+            elements.add(num);
+        }
+
+        Collections.sort(elements, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int freq1 = frequencyMap.get(o1);
+                int freq2 = frequencyMap.get(o2);
+                if (freq1 != freq2) {
+                    return freq2 - freq1; // Higher frequency comes first
+                } else {
+                    return o1 - o2; // If frequency is same, smaller element comes first
+                }
+            }
+        });
+
+        // Create result list from sorted elements
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int num : elements) {
+            result.add(num);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {5,5,4,6,4};
+        int n = arr.length;
+
+        sortByFreq(arr,n);
+    }
 }
