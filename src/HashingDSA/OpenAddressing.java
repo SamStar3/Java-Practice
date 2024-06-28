0c4dd2af0fbd676805ef374714c6036066abfd91
SamStar3 <samstar1044948@gmail.com>
2024-06-28 16:39:02 +0530

    file added

--- src/HashingDSA/OpenAddressing.java
+++ src/HashingDSA/OpenAddressing.java
@@ -1,8 +1,46 @@
 package HashingDSA;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class OpenAddressing {
 
+    static int longestSub(int[] arr, int n ) {
+//        Arrays.sort(arr);
+//        int res = 1;
+//        int cur = 1;
+//        for (int i = 0; i < n; i++ ){
+//            if (arr[i] == arr[i +1]) {
+//                cur++;
+//            }else if (arr[i] != arr[i - 1]) {
+//                res = Math.max(res, cur);
+//                cur = 1;
+//            }
+//        }
+//        return Math.max(res,cur);
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        int res = 1;
+        for (Integer num : set) {
+            if (set.contains(num - 1) == false) {
+                int cur = 1;
+                while (set.contains(num + cur)) {
+                    cur++;
+
+                }
+                res = Math.max(res, cur);
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
 
+        int[] arr = {2,9,4,3,10};
+        int n = arr.length;
+
+        System.out.println(longestSub(arr,n));
     }
 }
