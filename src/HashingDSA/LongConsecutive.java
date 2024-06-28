11dda135f891c9ed9b99ef33fb76b2c7a784f512
SamStar3 <samstar1044948@gmail.com>
2024-06-28 16:09:18 +0530

    file updated

--- src/HashingDSA/LongConsecutive.java
+++ src/HashingDSA/LongConsecutive.java
@@ -1,8 +1,31 @@
 package HashingDSA;
 
 public class LongConsecutive {
+    static int maxCommon(int[] arr1, int[] arr2, int n ) {
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum1 =0;
+            int sum2 =0;
+            for (int j = i; j < n; j++) {
+                sum1 += arr1[j];
+                sum2 += arr2[j];
+                if (sum1 == sum2) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
 
+        int[] arr1 = {0,1,0,0,0,0};
+        int[] arr2 = {1,0,1,0,0,1};
+
+        int n = arr2.length;
+
+        System.out.println(maxCommon(arr1, arr2,n));
     }
 }
--- src/HashingDSA/LongSub.java
+++ src/HashingDSA/LongSub.java
@@ -1,2 +1,33 @@
-package HashingDSA;public class LongSub {
+package HashingDSA;
+
+public class LongSub {
+
+    static int longSub(int[] arr, int n ) {
+
+        int res = 0;
+
+        for (int i = 0; i < n;  i++) {
+            int c0 = 0;
+            int c1 = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[j] == 0) {
+                    c0++;
+                }else {
+                    c1++;
+                }
+                if (c0 == c1) {
+                    res = Math.max(res, c0 + c1);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,1,0,1,0};
+        int n = arr.length;
+
+        System.out.println(longSub(arr, n));
+    }
 }
