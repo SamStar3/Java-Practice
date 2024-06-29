f5ed8bb0ee45a00597af571443dc6f9d58094a65
SamStar3 <samstar1044948@gmail.com>
2024-06-29 12:25:52 +0530

    file added

--- src/HashingDSA/NumContaining.java
+++ src/HashingDSA/NumContaining.java
@@ -2,7 +2,31 @@ package HashingDSA;
 
 public class NumContaining {
 
+    static void distWind(int[] arr, int k, int n) {
+
+        for (int i = 0; i <= n - k; i++) {
+            int count = 0;
+            for (int j = 0; j < k; j++){
+                boolean flag = false;
+                for (int p = 0; p < j; p++) {
+                    if (arr[i + j] == arr[i +p]) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
     public static void main(String[] args) {
 
+        int[] arr = {10,10,5,3,20,5};
+        int k = 4;
+        int n = arr.length;
+
+        distWind(arr, k, n);
     }
 }
