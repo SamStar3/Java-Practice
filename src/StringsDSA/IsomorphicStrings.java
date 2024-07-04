d4f8642622ee7462a46289d6c0c4f5e7af5a7bf7
SamStar3 <samstar1044948@gmail.com>
2024-07-04 20:04:13 +0530

    file added

--- src/StringsDSA/IsomorphicStrings.java
+++ src/StringsDSA/IsomorphicStrings.java
@@ -1,2 +1,54 @@
-package StringsDSA;public class IsomorphicStrings {
+package StringsDSA;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+
+    public static boolean areIsomorphic(String str1, String str2) {
+        // If lengths are not equal, they cannot be isomorphic
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Create two hash maps to store character mappings
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+
+        // Iterate through characters of both strings
+        for (int i = 0; i < str1.length(); i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+
+            // Check if there is a mapping already
+            if (map1.containsKey(c1)) {
+                // If c1 is already mapped to a different character, return false
+                if (map1.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                // Map c1 to c2
+                map1.put(c1, c2);
+            }
+
+            // Check the reverse mapping
+            if (map2.containsKey(c2)) {
+                // If c2 is already mapped to a different character, return false
+                if (map2.get(c2) != c1) {
+                    return false;
+                }
+            } else {
+                // Map c2 to c1
+                map2.put(c2, c1);
+            }
+        }
+
+        // If all characters can be mapped correctly, return true
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(areIsomorphic("aab", "xxy"));
+        System.out.println(areIsomorphic("aab", "xyz"));
+    }
 }
--- src/StringsDSA/StringsRotations.java
+++ src/StringsDSA/StringsRotations.java
@@ -1,2 +1,20 @@
-package StringsDSA;public class StringsRotations {
+package StringsDSA;
+
+public class StringsRotations {
+
+    public static boolean areRotations(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        String concatenated = s1 + s1;
+//        System.out.println(concatenated);
+        return concatenated.contains(s2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(areRotations("geeksforgeeks", "forgeeksgeeks"));
+//        areRotations("geeksforgeeks", "forgeeksgeeks");
+    }
+
 }
