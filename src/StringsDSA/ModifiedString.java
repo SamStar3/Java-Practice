5c5a8590e6070c479eee82581c9dc8444679d7e4
SamStar3 <samstar1044948@gmail.com>
2024-07-08 19:40:15 +0530

    file added

--- src/StringsDSA/ModifiedString.java
+++ src/StringsDSA/ModifiedString.java
@@ -1,2 +1,33 @@
-package StringsDSA;public class ModifiedString {
+package StringsDSA;
+
+public class ModifiedString {
+
+    static int modified(String str) {
+        int n = str.length();
+        if (n < 3 ) {
+            return 0;
+        }
+        int insert = 0;
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+                if (count == 3) {
+                    insert++;
+                    count = 1;
+                }
+            }else {
+                count = 1;
+            }
+        }
+        return insert;
+    }
+
+    public static void main(String[] args ) {
+
+        String str = "aabbbcc";
+
+        System.out.println(modified(str));
+    }
 }
