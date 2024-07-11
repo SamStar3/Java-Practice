e836cb5859ae95813f9284a5c4130ad184e2b67e
SamStar3 <samstar1044948@gmail.com>
2024-07-11 17:21:26 +0530

    file added

--- src/StringsDSA/Lexicographic.java
+++ src/StringsDSA/Lexicographic.java
@@ -1,2 +1,34 @@
-package StringsDSA;public class Lexicographic {
+package StringsDSA;
+
+public class Lexicographic {
+
+    static final int CHAR = 256;
+    static int lexRank(String str) {
+        int res = 1;
+        int n = str.length();
+        int mul = 1000000007;
+        int count[] = new int[CHAR];
+
+        for (int i = 0; i < n; i++) {
+            count[str.charAt(i)]++;
+        }
+        for (int i = 1; i < CHAR; i++) {
+            count[i] += count[i - 1];
+        }
+        for (int i = 0; i < n -1; i++) {
+            mul = mul /(n - 1);
+            res = res + count[str.charAt(i) -1] * mul;
+            for (int j = str.charAt(i); j < CHAR;j++) {
+                count[j]--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "STRING";
+
+        System.out.println(lexRank(str));
+    }
 }
