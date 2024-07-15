02cdabf05f701edc8a998564a83a9689d99e5637
SamStar3 <samstar1044948@gmail.com>
2024-07-15 22:00:08 +0530

    file added

--- src/LinkedListDSA/Traversing.java
+++ src/LinkedListDSA/Traversing.java
@@ -1,4 +1,29 @@
 package LinkedListDSA;
 
 public class Traversing {
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int x) {
+            data = x;
+            next = null;
+        }
+    }
+
+    public static void printList(Node head){
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        printList(head);
+    }
 }
