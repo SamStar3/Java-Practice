d48b7c844b2306738f2dbef7d17a4c6d1d5238e3
SamStar3 <samstar1044948@gmail.com>
2024-07-23 17:03:34 +0530

    file added

--- src/LinkedListDSA/Recursive.java
+++ src/LinkedListDSA/Recursive.java
@@ -2,7 +2,36 @@ package LinkedListDSA;
 
 public class Recursive {
 
-    public static void main(String[] args) {
+    /*void addNode(Node head_ref, int pos, int data)
+    {
+        // Your code here
+        Node newNode = new Node(data);
 
-    }
+        // If the list is empty
+        if (head_ref == null) {
+            head_ref = newNode;
+            return;
+        }
+
+        // Traverse the list to find the node at position `pos`
+        Node current = head_ref;
+        for (int i = 0; i < pos; i++) {
+            if (current != null) {
+                current = current.next;
+            } else {
+                // If `pos` is greater than the length of the list, do nothing
+                return;
+            }
+        }
+
+        // Adjust the pointers to insert the new node after the `p`th node
+        newNode.next = current.next;
+        newNode.prev = current;
+
+        if (current.next != null) {
+            current.next.prev = newNode;
+        }
+
+        current.next = newNode;
+    }*/
 }
