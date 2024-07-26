3705df80532f31ce7ddf420a037a210b6f9d6315
SamStar3 <samstar1044948@gmail.com>
2024-07-26 21:51:19 +0530

    file added

--- src/LinkedListDSA/CircularLinked.java
+++ src/LinkedListDSA/CircularLinked.java
@@ -1,4 +1,21 @@
 package LinkedListDSA;
 
 public class CircularLinked {
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+//    static Node head;
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(5);
+        head.next.next = new Node(20);
+        head.next.next.next = new Node(15);
+        head.next.next.next.next = head;
+    }
 }
