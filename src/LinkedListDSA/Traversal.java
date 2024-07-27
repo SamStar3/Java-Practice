5c0618f730fb0e2c459b50eba36896529e975377
SamStar3 <samstar1044948@gmail.com>
2024-07-27 15:55:27 +0530

    file added

--- src/LinkedListDSA/Traversal.java
+++ src/LinkedListDSA/Traversal.java
@@ -1,2 +1,20 @@
-package LinkedListDSA;public class Traversal {
+package LinkedListDSA;
+
+public class Traversal {
+
+    static void printList(Node head) {
+        if (head == null) {
+            return;
+        }
+//        System.out.print(head.data + " ");
+//        for (Node r = head.next; r != head; r = r.next) {
+//            System.out.print(r.data + " ");
+//        }
+
+        Node r = head;
+        do {
+            System.out.print(r.data + " ");
+            r = r.next;
+        }while (r != head);
+    }
 }
