03d5426a074af15c55ac53c45e3074c53bc80357
SamStar3 <samstar1044948@gmail.com>
2024-07-27 16:46:00 +0530

    file added

--- src/LinkedListDSA/InsertEndCirc.java
+++ src/LinkedListDSA/InsertEndCirc.java
@@ -1,2 +1,31 @@
-package LinkedListDSA;public class InsertEndCirc {
+package LinkedListDSA;
+
+public class InsertEndCirc {
+
+    static Node insertEnd(Node head, int x) {
+        Node temp = new Node(x);
+        if (head == null) {
+            temp.next = temp;
+            return temp;
+        }else {
+            Node curr = head;
+            while (curr.next != head) {
+                curr =curr.next;
+            }
+            curr.next = temp;
+            temp.next = head;
+            return head;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=head;
+        head=insertEnd(head,15);
+//        printlist(head);
+
+    }
 }
