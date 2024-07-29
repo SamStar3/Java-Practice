c47b57aa816951157ab459f11ccc02bc13accb99
SamStar3 <samstar1044948@gmail.com>
2024-07-29 21:53:57 +0530

    file added

--- src/LinkedListDSA/DeleteHeadCir.java
+++ src/LinkedListDSA/DeleteHeadCir.java
@@ -1,4 +1,19 @@
 package LinkedListDSA;
 
 public class DeleteHeadCir {
+
+    Node delHead(Node head ) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == head) {
+            return null;
+        }
+        Node curr = head;
+        while (curr.next != head) {
+            curr = curr.next;
+        }
+        curr.next =head.next;
+        return curr.next;
+    }
 }
