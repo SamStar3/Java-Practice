56ab5ee1e036b3f05433c4c9874903fb36f8a4c8
SamStar3 <samstar1044948@gmail.com>
2024-08-03 21:03:06 +0530

    file added

--- src/ArrayLists/Iterators.java
+++ src/ArrayLists/Iterators.java
@@ -1,4 +1,18 @@
 package ArrayLists;
 
 public class Iterators {
+
+//    private static void testIterator(LinkedList<String> list) {
+//        var iterator = list.iterator();
+//        while(iterator.hasNext()) {
+////            System.out.println(iterator.next);
+//            if (iterator.next().equals("Brisbane")){
+//                iterator.add("Lake Wivenhoe");
+//            }
+//        }
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.previous());
+//        }
+//        System.out.println(list);
+//    }
 }
--- src/ArrayLists/Playlist.java
+++ src/ArrayLists/Playlist.java
@@ -2,4 +2,6 @@ package ArrayLists;
 
 public class Playlist {
 
+
+
 }
