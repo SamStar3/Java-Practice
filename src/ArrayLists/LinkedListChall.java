039b97b6e98a049c23be4b2390384c108d8defbc
SamStar3 <samstar1044948@gmail.com>
2024-08-06 10:37:00 +0530

    file added

--- src/ArrayLists/LinkedListChall.java
+++ src/ArrayLists/LinkedListChall.java
@@ -1,6 +1,45 @@
 package ArrayLists;
 
+//record Place(String name, int distance){
+//
+//    @Override
+//    public String toString() {
+//        return String.format("%s (%d)",name,distance);
+//    }
+//}
 public class LinkedListChall {
-
-
+//
+//    public static void main(String[] args) {
+//
+//        LinkedList<Place> placesToVisit = new LinkedList<>();
+//
+//        Place adelaide = new Place("Adelaid",1374);
+//        addPlace(placesToVisit,adelaide);
+//
+//        System.out.println(placesToVisit);
+//    }
+//
+//    private static void addPlace(LinkedList<Place> list, Place place){
+//        if (list.contains(place)){
+//            System.out.println("Fount duplicate" + place);
+//            return;
+//        }
+//
+//        for (Place p : list) {
+//            if (p.name().equalsIgnoreCase(place.name())){
+//                System.out.println("Found duplicate: " + place);
+//                return;
+//            }
+//        }
+//
+//        int matchedIndex = 0;
+//        for (var listPlace : list) {
+//            if (place.distance() < listPlace.distance()) {
+//                list.add(matchedIndex, place);
+//                return;
+//            }
+//            matchedIndex++;
+//        }
+//        list.add(place);
+//    }
 }
