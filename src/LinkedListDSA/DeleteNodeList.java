30bcb0696cfea6c3500078eda625967400d1b94b
SamStar3 <samstar1044948@gmail.com>
2024-08-29 21:00:18 +0530

    file added

--- src/LinkedListDSA/DeleteNodeList.java
+++ src/LinkedListDSA/DeleteNodeList.java
@@ -1,2 +1,37 @@
-package LinkedListDSA;public class DeleteNodeList {
+package LinkedListDSA;
+
+public class DeleteNodeList {
+//    public Node deleteNode(Node head, int x) {
+//    if (head == null || x <= 0) return head;
+//
+//    Node current = head;
+//
+//    // Traverse to the node at position x
+//    for (int i = 1; current != null && i < x; i++) {
+//        current = current.next;
+//    }
+//
+//    // If the node to be deleted is not found, return the head
+//    if (current == null) return head;
+
+    // If the node to be deleted is the head node
+//    if (current == head) {
+//        head = current.next;
+//        if (head != null) {
+//            head.prev = null;
+//        }
+//    } else {
+//        // Change the next of the previous node
+//        if (current.prev != null) {
+//            current.prev.next = current.next;
+//        }
+//
+//        // Change the prev of the next node
+//        if (current.next != null) {
+//            current.next.prev = current.prev;
+//        }
+//    }
+
+//    return head;
+//}
 }
