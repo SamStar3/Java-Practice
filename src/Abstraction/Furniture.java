b78e8299463e9bd3a035c9e4a632cf9154b02375
SamStar3 <samstar1044948@gmail.com>
2024-09-01 19:07:52 +0530

    file added

--- src/Abstraction/Furniture.java
+++ src/Abstraction/Furniture.java
@@ -1,15 +1,15 @@
 package Abstraction;
 
-public class ArtObject extends ProductForSale{
+public class Furniture extends ProductForSale{
 
-    public ArtObject(String type, double price, String description) {
+    public Furniture(String type, double price, String description) {
         super(type, price, description);
     }
 
     @Override
     public void showDetails() {
 
-        System.out.println("This " + type + " is a beautiful reproduction");
+        System.out.println("This " + type + " was manufactured in North Carolina");
         System.out.printf("The price of the piece is $%6.2f %n", price);
         System.out.println(description);
     }
--- src/Abstraction/ProductForSale.java
+++ src/Abstraction/ProductForSale.java
@@ -1,2 +1,25 @@
-package Abstraction;public class ProductForSale {
+package Abstraction;
+
+public abstract class ProductForSale {
+
+    protected String type;
+    protected double price;
+    protected String description;
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+    public double getSalesPrice(int qty) {
+        return qty * price;
+    }
+
+    public void printPricedItem(int qty){
+        System.out.printf("%2 qty at $%8.2f each, %-15s %-35s %n",
+                qty, price, type, description);
+    }
+
+    public abstract void showDetails();
+
 }
