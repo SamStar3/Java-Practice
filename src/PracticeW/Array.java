81d58c6e181b70e8e8bec2550e2b8d9c040722df
SamStar3 <samstar1044948@gmail.com>
2024-09-07 16:30:32 +0530

    file added

--- src/PracticeW/Array.java
+++ src/PracticeW/Array.java
@@ -6,6 +6,24 @@ public class Array {
 
     public static int minStepsToOne(int n) {
 
+        int[] dp = new int[n + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        return dp[n];
     }
     public static void main(String[] args) {
         // Create a scanner object to read input
@@ -16,7 +34,7 @@ public class Array {
         int n = scanner.nextInt();
 
         // Output the minimum steps to reduce n to 1
-        System.out.println("Minimum steps to reduce " + n + " to 1: " + minStepsToOne(n));
+        System.out.println("Minimum steps to reduce " + n + " to 1 : " + minStepsToOne(n));
 
         // Close the scanner
         scanner.close();
--- src/PracticeW/Prime.java
+++ src/PracticeW/Prime.java
@@ -1,4 +1,35 @@
 package PracticeW;
 
+import java.util.Scanner;
+
 public class Prime {
+
+    public static int countDigits(int n){
+
+        int count = 0;
+
+         if (n == 0){
+             return 1;
+         }
+         while (n != 0) {
+             n = n/10;
+             count++;
+
+         }
+         return count;
+    }
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the input integer n
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        // Output the number of digits using mathematical approach
+        System.out.println("Number of digits in " + n + " is: " + countDigits(n));
+
+        // Close the scanner
+        scanner.close();
+    }
 }
