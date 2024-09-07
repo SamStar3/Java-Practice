65b2510c5711d56a1338a72a873cde6cef11bdb2
SamStar3 <samstar1044948@gmail.com>
2024-09-07 16:08:12 +0530

    file added

--- src/PracticeW/Array.java
+++ src/PracticeW/Array.java
@@ -1,8 +1,24 @@
 package PracticeW;
 
+import java.util.Scanner;
+
 public class Array {
 
-    public static void main(String[] args){
+    public static int minStepsToOne(int n) {
 
+    }
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the input integer n
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        // Output the minimum steps to reduce n to 1
+        System.out.println("Minimum steps to reduce " + n + " to 1: " + minStepsToOne(n));
+
+        // Close the scanner
+        scanner.close();
     }
 }
--- src/PracticeW/Fun.java
+++ src/PracticeW/Fun.java
@@ -1,4 +1,29 @@
 package PracticeW;
 
+import java.util.Scanner;
+
 public class Fun {
+
+    public static int sumOfEvenNumbers(int n){
+        int Total = 0;
+
+        for (int i = 0; i <= n; i += 2) {
+            Total += i;
+        }
+        return Total;
+    }
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the input integer n
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        // Output the result
+        System.out.println("Sum of all even numbers from 1 to " + n + " is: " + sumOfEvenNumbers(n));
+
+        // Close the scanner
+        scanner.close();
+    }
 }
