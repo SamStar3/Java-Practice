9732ce6627650a81014a66b7663fe5a7e544081b
SamStar3 <samstar1044948@gmail.com>
2024-09-12 23:55:57 +0530

    file added

--- src/StackDSA/MyStack.java
+++ src/StackDSA/MyStack.java
@@ -45,24 +45,27 @@ public class MyStack {
 //        System.out.print(" "+ a[i]);
 //    }
 
-    ArrayList<Integer> al = new ArrayList<>();
+//    ArrayList<Integer> al = new ArrayList<>();
+//
+//    void push(int x) {
+//        al.add(x);
+//    }
+//
+//    int pop(){
+//        int res = al.get(al.size()-1);
+//        al.remove(al.size()-1);
+//        return res;
+//    }
+//    int peek(){
+//        return al.get(al.size()-1);
+//    }
+//    boolean isEmpty(){
+//        return al.isEmpty();
+//    }
+//    int size(){
+//        return al.size();
+//    }
 
-    void push(int x) {
-        al.add(x);
-    }
 
-    int pop(){
-        int res = al.get(al.size()-1);
-        al.remove(al.size()-1);
-        return res;
-    }
-    int peek(){
-        return al.get(al.size()-1);
-    }
-    boolean isEmpty(){
-        return al.isEmpty();
-    }
-    int size(){
-        return al.size();
-    }
+
 }
