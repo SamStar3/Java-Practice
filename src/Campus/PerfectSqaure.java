1001d018e40d16e8ec33c4ea7e3762c52f52be28
SamStar3 <samstar1044948@gmail.com>
2024-09-26 17:34:14 +0530

    file added

--- src/Campus/PerfectSqaure.java
+++ src/Campus/PerfectSqaure.java
@@ -1,2 +1,18 @@
-package Campus;public class PerfectSqaure {
+package Campus;
+
+import java.util.Scanner;
+
+public class PerfectSqaure {
+
+    static void isPerfect(int sum) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = sc.nextInt();
+        isPerfect(sum);
+    }
+
 }
