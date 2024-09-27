d6b3b96d32f3e9e72ee1f2e0940b711495abca47
SamStar3 <samstar1044948@gmail.com>
2024-09-27 12:36:23 +0530

    File added

--- src/Campus/HIghColorContainer.java
+++ src/Campus/HIghColorContainer.java
@@ -1,2 +1,34 @@
-package Campus;public class HIghColorContainer {
+package Campus;
+
+import java.util.Arrays;
+
+public class HIghColorContainer {
+
+    public static void main(String[] args) {
+
+        String st = "bbbaaababa";
+        int chunkSize = 3;
+        int temp = 0;
+        int count = 0;
+        for (int i = 0; i < st.length(); i += chunkSize) {
+            int end = Math.min(i + chunkSize, st.length());
+            String chunk = st.substring(i, end);
+//            System.out.println(chunk);
+            for(int j=0;j<chunk.length();j++)
+            {
+                if(chunk.charAt(j) == 'a')
+                {
+                    count++;
+                }
+            }
+
+            if(count > temp){
+                temp = count;
+            }
+
+            count=0;
+        }
+
+        System.out.print(temp);
+    }
 }
