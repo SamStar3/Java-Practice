b2b00b4beab685f6a958eccf56951d2d632c8dd1
SamStar3 <samstar1044948@gmail.com>
2024-09-28 10:30:00 +0530

    File added

--- src/Campus/ReverseString.java
+++ src/Campus/ReverseString.java
@@ -1,2 +1,32 @@
-package Campus;public class ReverseString {
+package Campus;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String a ="";
+
+        String input = "Hello World";
+        String[] words = input.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            String reversedWord = new StringBuilder(word).reverse().toString();
+            String capitalizedWord = reversedWord.substring(0, 1).toUpperCase() + reversedWord.substring(1).toLowerCase();
+            result.append(capitalizedWord).append(" ");
+        }
+
+        System.out.println(result.toString().trim());
+
+
+
+       /* for (int i = 0; i < str.length(); i++ ) {
+            if (str.charAt(i) == ' ' || i == str.length() - 1) {
+                for (int j = i - 1; j >= 0; j--) {
+                    System.out.print(str.charAt(j));
+                    i--;
+                }
+            }
+        }*/
+    }
 }
