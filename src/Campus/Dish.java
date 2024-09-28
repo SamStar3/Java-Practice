4fdf623f4accd8ad59c78a032bc83c0b18120f2e
SamStar3 <samstar1044948@gmail.com>
2024-09-28 14:45:32 +0530

    File added

--- src/Campus/Dish.java
+++ src/Campus/Dish.java
@@ -1,4 +1,18 @@
 package Campus;
 
 public class Dish {
+
+    public static void main(String[] args){
+
+        int[] arr = {-1, 2, 3};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++ ){
+            if (arr[i] > 0) {
+                sum += arr[i] * (i + 1);
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
