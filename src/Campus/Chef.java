9fc151972411f7acbddd6b4a27336b195bc30286
SamStar3 <samstar1044948@gmail.com>
2024-10-01 10:56:38 +0530

    file addeD

--- src/Campus/Chef.java
+++ src/Campus/Chef.java
@@ -1,2 +1,34 @@
-package Campus;public class Chef {
+package Campus;
+
+public class Chef {
+
+    public static void main(String[] args) {
+
+//        int[] arr1 = {2,20};
+//        int[] arr2 = {15, 67};
+//        int[] arr3 = {10, 90};
+//        int res = 0;
+//
+//        for (int i = 0; i < arr2.length; i++) {
+//            if(arr1[1] >= arr2[i]){
+//                res += arr3[i];
+//            }
+//        }
+//        System.out.println(res);
+
+
+        int[][] arr = {
+                {2, 20},
+                {15, 67},
+                {10,90}
+        };
+        int res = 0;
+        for (int i = 0; i < arr.length - 1; i++){
+            if (arr[1][i] >= arr[0][1]){
+                res += arr[2][i];
+            }
+        }
+        System.out.println(res);
+        
+    }
 }
--- src/Campus/ReverseInWhile.java
+++ src/Campus/ReverseInWhile.java
@@ -1,2 +1,15 @@
-package Campus;public class ReverseInWhile {
+package Campus;
+
+public class ReverseInWhile {
+
+    public static void main(String[] args){
+
+        String str = "Hello World";
+
+        int len = str.length() - 1;
+        while (len >= 0) {
+            System.out.print(str.charAt(len));
+            len--;
+        }
+    }
 }
