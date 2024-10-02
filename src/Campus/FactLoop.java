bea4668b22270feee7684281db7b30684f193a29
SamStar3 <samstar1044948@gmail.com>
2024-10-03 02:39:59 +0530

    file addeD

--- src/Campus/FactLoop.java
+++ src/Campus/FactLoop.java
@@ -1,2 +1,16 @@
-package Campus;public class FactLoop {
+package Campus;
+
+public class FactLoop {
+
+    public static void main(String[] args){
+        int n=5;
+        int fact = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+            System.out.println(fact);
+        }
+
+        System.out.println(fact);
+    }
 }
