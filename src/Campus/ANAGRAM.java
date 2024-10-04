f981c8eff5d855064965d7fbaa11e698cbef6310
SamStar3 <samstar1044948@gmail.com>
2024-10-04 21:07:57 +0530

    File added

--- src/Campus/ANAGRAM.java
+++ src/Campus/ANAGRAM.java
@@ -1,2 +1,23 @@
-package Campus;public class ANAGRAM {
+package Campus;
+import java.util.*;
+public class ANAGRAM {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        n = n.toLowerCase();
+        String k= sc.nextLine();
+        k = k.toLowerCase();
+
+        int l1 = n.length();
+        int l2 = k.length();
+
+        if(l1!=l2){
+            System.out.println("not a anagram");
+        }
+        else{
+            char arr[] = n.toCharArray();
+            Arrays.sort(arr);
+        }
+
+    }
 }
--- src/Campus/Palindrome.java
+++ src/Campus/Palindrome.java
@@ -1,4 +1,23 @@
 package Campus;
-
+import java.util.*;
 public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int check = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int r = n%10;
+            sum = (sum * 10) + r;
+            n =  n/10;
+        }
+        if(sum == check){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+    }
 }
