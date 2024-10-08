e1afbe6ff1c2956c66f3efca98a8f662c1d9396c
SamStar3 <samstar1044948@gmail.com>
2024-10-08 10:43:35 +0530

    file added

--- src/Campus/RecursionPrimeFactor.java
+++ src/Campus/RecursionPrimeFactor.java
@@ -1,2 +1,24 @@
-package Campus;public class RecursionPrimeFactor {
+package Campus;
+
+public class RecursionPrimeFactor {
+
+    static void primeFactor(int n)
+    {
+       if(n==1)
+       {
+           return;
+       }
+       int i=2;
+       while (n%i != 0)
+       {
+           i = i+1;
+       }
+       System.out.print(STR."\{i} ");
+       primeFactor(n/i);
+
+    }
+    public static void main(String[] arg)
+    {
+       primeFactor(100);
+    }
 }
