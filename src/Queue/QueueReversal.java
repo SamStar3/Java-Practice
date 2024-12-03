8325e013b1aed662536365f2d98ea2a363a1356f
SamStar3 <samstar1044948@gmail.com>
2024-12-03 14:14:55 +0530

    file addded

--- src/Queue/QueueReversal.java
+++ src/Queue/QueueReversal.java
@@ -1,2 +1,33 @@
-package Queue;public class QueueReversal {
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
+public class QueueReversal {
+
+    static Queue<Integer> rev(Queue<Integer> q){
+        //add code here.
+
+        Stack<Integer> s = new Stack<>();
+
+        while(!q.isEmpty()){
+            s.push(q.poll());
+        }
+
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+        return q;
+    }
+
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+
+        System.out.println(rev(q));
+    }
 }
--- src/Queue/ReverseFirstK.java
+++ src/Queue/ReverseFirstK.java
@@ -1,8 +1,56 @@
 package Queue;
 
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
+
 public class ReverseFirstK {
 
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        // add code here.
+
+        Stack<Integer> s = new Stack<>();
+
+        int i = 0;
+
+        while(i!=k){
+
+            s.push(q.remove());
+            i++;
+        }
+
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+
+        int rem = q.size() - k;
+
+        for(int j = 0; j < rem; j++){
+            q.add(q.poll());
+        }
+        return q;
+    }
     public static void main(String[] args){
+
+
+        ReverseFirstK gfg = new ReverseFirstK();
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Example Input
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 3;
+
+        // Add elements to the queue
+        for (int num : arr) {
+            queue.add(num);
+        }
+
+        System.out.println("Original Queue: " + queue);
+
+        Queue<Integer> modifiedQueue = gfg.modifyQueue(queue, k);
+
+        System.out.println("Modified Queue: " + modifiedQueue);
 
     }
 }
