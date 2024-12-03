40ceb5b0dad5f6cbbdc643196d20fc861f8cacbb
SamStar3 <samstar1044948@gmail.com>
2024-12-03 13:41:42 +0530

    file addded

--- src/Queue/ReversingQueue.java
+++ src/Queue/ReversingQueue.java
@@ -1,8 +1,27 @@
 package Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 public class ReversingQueue {
 
-    public static void main(String[] args){
+    static void reverse(Queue<Integer> q){
+        if (q.isEmpty()){
+            return;
+        }
+        int x = q.remove();
+        reverse(q);
+        q.offer(x);
 
+        System.out.println(q);
+    }
+
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+
+        reverse(q);
     }
 }
--- src/Queue/StackQueue.java
+++ src/Queue/StackQueue.java
@@ -1,10 +1,34 @@
 package Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class StackQueue {
+
+    static void reverse(Queue<Integer> queue){
+
+        Stack<Integer> s = null;
+
+        while (queue.isEmpty()== false){
+            s.push(queue.remove());
+        }
+
+        while (s.isEmpty() == false){
+            queue.add(s.pop());
+        }
+        System.out.println(queue);
+    }
 
 
     public static void main(String[] args){
 
+        Queue<Integer> q = new LinkedList<>();
 
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+
+        reverse(q);
     }
 }
