5042ce66a662ede029ab044f3d40f7c88df31a4f
SamStar3 <samstar1044948@gmail.com>
2024-12-04 12:45:52 +0530

    file addded

--- src/Deque/Test.java
+++ src/Deque/Test.java
@@ -1,2 +1,28 @@
-package Deque;public class Test {
+package Deque;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        Deque<Integer> d = new LinkedList<>();
+
+        d.addFirst(5);
+        d.addLast(20);
+        d.addFirst(0);
+        d.addLast(30);
+
+        for (int num : d){
+            System.out.print(num + " " );
+        }
+
+        System.out.println(d.getFirst());
+        System.out.println(d.getLast());
+        d.removeFirst();
+        d.removeLast();
+        System.out.println(d.getFirst());
+        System.out.println(d.getLast());
+    }
 }
