9322fe42c21b8875398d83ece77cc971f897bbd9
SamStar3 <samstar1044948@gmail.com>
2024-12-05 22:48:40 +0530

    file addded

--- src/Deque/Test1.java
+++ src/Deque/Test1.java
@@ -1,2 +1,29 @@
-package Deque;public class Test1 {
+package Deque;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Test1 {
+
+    public static void main(String[] args)
+    {
+        // Initializing an deque
+        Deque<String> dq
+                = new ArrayDeque<String>();
+
+        // add() method to insert
+        dq.add("For");
+        dq.addFirst("Geeks");
+        dq.addLast("Geeks");
+
+        System.out.println(dq);
+
+        System.out.println(dq.pop());
+
+        System.out.println(dq.poll());
+
+        System.out.println(dq.pollFirst());
+
+        System.out.println(dq.pollLast());
+    }
 }
