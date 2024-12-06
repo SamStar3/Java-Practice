9c6850df35fe4b0a5f26e9f790b9f502aab7b816
SamStar3 <samstar1044948@gmail.com>
2024-12-06 16:35:45 +0530

    file added

--- src/Deque/DequeDeletion.java
+++ src/Deque/DequeDeletion.java
@@ -1,2 +1,47 @@
-package Deque;public class DequeDeletion {
+package Deque;
+
+import java.util.Iterator;
+import java.util.ArrayDeque;
+
+public class DequeDeletion {
+
+    public static void eraseAt(ArrayDeque<Integer> deq, int X) {
+        // Your code here
+
+        if(X >= 0 && X < deq.size()){
+
+            int index = 0;
+            Iterator<Integer> it = deq.iterator();
+
+            while(it.hasNext()){
+                it.next();
+                if(index == X){
+                    it.remove();
+                    break;
+                }
+                index++;
+            }
+        }
+    }
+
+    public static void eraseInRange(ArrayDeque<Integer> deq, int start, int end) {
+        if(start >= 0 && start < end && end <= deq.size()){
+            int index = 0;
+            Iterator<Integer> it = deq.iterator();
+            while(it.hasNext()){
+                it.next();
+                if(index >= start && index < end ){
+                    it.remove();
+                }
+                if(index >= end){
+                    break;
+                }
+                index++;
+            }
+        }
+    }
+
+    public static void eraseAll(ArrayDeque<Integer> deq) {
+        deq.clear();
+    }
 }
