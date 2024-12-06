63ac35a765b3697c5451c65fde396aa3881d950d
SamStar3 <samstar1044948@gmail.com>
2024-12-06 16:46:41 +0530

    file added

--- src/Deque/DequeTraversal.java
+++ src/Deque/DequeTraversal.java
@@ -1,2 +1,19 @@
-package Deque;public class DequeTraversal {
+package Deque;
+
+import java.util.Iterator;
+import java.util.ArrayDeque;
+
+public class DequeTraversal {
+
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        Iterator<Integer> it = deq.iterator();
+
+        // Traverse and print elements using the iterator
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+
+        // Print a newline at the end
+        System.out.println();
+    }
 }
