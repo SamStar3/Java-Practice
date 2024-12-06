73abf352a8f8d2237368b6f4a3cbf9cbf75dd0b5
SamStar3 <samstar1044948@gmail.com>
2024-12-06 15:24:39 +0530

    file added

--- src/Deque/Test3.java
+++ src/Deque/Test3.java
@@ -1,2 +1,16 @@
-package Deque;public class Test3 {
+package Deque;
+
+import java.util.ArrayDeque;
+public class Test3 {
+
+    public static ArrayDeque<Integer> deque_Init(int arr[], int n) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            deque.addLast(arr[i]);
+        }
+
+        return deque;
+    }
 }
