27949f7388328f2319c593c6e3c6816160681c75
SamStar3 <samstar1044948@gmail.com>
2024-12-12 23:08:09 +0530

    file added

--- src/Tree/PostorderTraversal.java
+++ src/Tree/PostorderTraversal.java
@@ -1,8 +1,33 @@
 package Tree;
 
+import java.util.ArrayList;
+
 public class PostorderTraversal {
+
+    static ArrayList<Integer> postOrder(Node root){
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null){
+            return result;
+        }
+
+        result.addAll(postOrder(root.left));
+        result.addAll(postOrder(root.right));
+        result.add(root.key);
+
+        return result;
+    }
 
     public static void main(String[] args){
 
+        Node root = new Node(1);
+        root.left = new Node(4);
+        root.left.left = new Node(4);
+        root.left.right = new Node(2);
+
+        ArrayList<Integer> result = postOrder(root);
+
+        System.out.println(result);
     }
 }
