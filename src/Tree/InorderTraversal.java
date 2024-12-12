887e3e0c635ce28b68e25a5c62650069a9227a60
SamStar3 <samstar1044948@gmail.com>
2024-12-12 22:56:32 +0530

    file added

--- src/Tree/InorderTraversal.java
+++ src/Tree/InorderTraversal.java
@@ -1,8 +1,35 @@
 package Tree;
 
+import dev.lpa.Array;
+
+import java.util.ArrayList;
+
 public class InorderTraversal {
 
+    static ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null){
+            return result;
+        }
+
+        result.addAll(inOrder(root.left));
+
+        result.add(root.key);
+
+        result.addAll(inOrder(root.right));
+
+        return result;
+    }
     public static void main(String[] args){
 
+        Node root = new Node(1);
+        root.left = new Node(4);
+        root.left.left = new Node(4);
+        root.left.right = new Node(2);
+
+        ArrayList<Integer> result = inOrder(root);
+
+        System.out.println(result);
     }
 }
