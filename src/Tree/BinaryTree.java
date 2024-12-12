f052df16530c69d94ccc28973075f9f9abac1234
SamStar3 <samstar1044948@gmail.com>
2024-12-12 21:57:44 +0530

    file added

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -1,8 +1,53 @@
 package Tree;
 
+
+class Node {
+
+    int key;
+
+    Node left, right;
+
+    public Node(int item)
+
+    {
+
+        key = item;
+
+        left = right = null;
+    }
+}
+
 public class BinaryTree {
 
-    public static void main(String[] args) {
+
+    Node root;
+
+
+    BinaryTree(int key) { root = new Node(key); }
+
+    BinaryTree() { root = null; }
+
+    public static void main(String[] args)
+
+    {
+
+        BinaryTree tree = new BinaryTree();
+
+
+        tree.root = new Node(1);
+
+
+
+        tree.root.left = new Node(2);
+
+        tree.root.right = new Node(3);
+
+
+
+        tree.root.left.left = new Node(4);
+
+        System.out.println(tree);
 
     }
 }
+
