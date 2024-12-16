36a266d97fa2aa2894e70b06eb7851c72663f466
SamStar3 <samstar1044948@gmail.com>
2024-12-17 00:16:38 +0530

    file added

--- src/Tree/LeftViewOfTree.java
+++ src/Tree/LeftViewOfTree.java
@@ -1,2 +1,41 @@
-package Tree;public class LeftViewOfTree {
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeftViewOfTree {
+
+    void printLift(Node root){
+        if (root == null){
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+
+        while (!q.isEmpty()){
+            int count = q.size();
+
+            for (int i = 0; i < count; i++){
+
+                Node curr = q.poll();
+
+                if (i == 0){
+                    System.out.print(curr.key);
+                }
+
+                if (curr.left != null){
+                    q.add(curr.left);
+                }
+                if (curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
 }
