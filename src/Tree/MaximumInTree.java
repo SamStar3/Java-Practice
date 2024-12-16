b0d752b6b804557134d434164df23d24b5e8f17c
SamStar3 <samstar1044948@gmail.com>
2024-12-16 21:34:35 +0530

    file added

--- src/Tree/MaximumInTree.java
+++ src/Tree/MaximumInTree.java
@@ -1,2 +1,28 @@
-package Tree;public class MaximumInTree {
+package Tree;
+
+public class MaximumInTree {
+
+    Node root;
+
+    int getMax(Node root){
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        return Math.max(root.key, Math.max(getMax(root.left), getMax(root.right)));
+    }
+    public static void main(String[] args){
+        MaximumInTree tree = new MaximumInTree();
+        tree.root = new Node(2);
+        tree.root.left = new Node(7);
+        tree.root.right = new Node(5);
+        tree.root.left.right = new Node(6);
+        tree.root.left.right.left = new Node(1);
+        tree.root.left.right.right = new Node(11);
+        tree.root.right.right = new Node(9);
+        tree.root.right.right.left = new Node(4);
+
+        // Function call
+        System.out.println("Maximum element is "
+                + tree.getMax(tree.root));
+    }
 }
