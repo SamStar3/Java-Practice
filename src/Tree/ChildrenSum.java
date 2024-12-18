d25e871f739a7d7c3f133dedaf36e749724921d9
SamStar3 <samstar1044948@gmail.com>
2024-12-18 22:50:13 +0530

    file added

--- src/Tree/ChildrenSum.java
+++ src/Tree/ChildrenSum.java
@@ -1,2 +1,24 @@
-package Tree;public class ChildrenSum {
+package Tree;
+
+public class ChildrenSum {
+
+    public static int isSumProperty(Node root)
+    {
+        // add your code here
+        if (root == null || (root.left == null && root.right == null)) {
+            return 1;
+        }
+
+        int leftData = (root.left != null) ? root.left.key : 0;
+        int rightData = (root.right != null) ? root.right.key : 0;
+
+        if (root.key == leftData + rightData) {
+            int leftResult = isSumProperty(root.left);
+            int rightResult = isSumProperty(root.right);
+
+            return (leftResult == 1 && rightResult == 1) ? 1 : 0;
+        } else {
+            return 0;
+        }
+    }
 }
