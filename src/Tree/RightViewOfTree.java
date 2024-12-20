581d6c0c704b29b432adcd17900d2f85966bb603
SamStar3 <samstar1044948@gmail.com>
2024-12-20 21:11:19 +0530

    file added

--- src/Tree/RightViewOfTree.java
+++ src/Tree/RightViewOfTree.java
@@ -1,2 +1,42 @@
-package Tree;public class RightViewOfTree {
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RightViewOfTree {
+
+    ArrayList<Integer> rightView(Node root) {
+        // add code here.
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(root == null){
+            return result;
+        }
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+
+            int count = q.size();
+
+            for(int i = 0; i < count; i++){
+
+                Node curr = q.poll();
+
+                if(i == count -1){
+                    result.add(curr.key);
+                }
+                if(curr.left != null) {
+                    q.add(curr.left);
+                }
+                if(curr.right != null ) {
+                    q.add(curr.right);
+                }
+            }
+        }
+        return result;
+    }
 }
