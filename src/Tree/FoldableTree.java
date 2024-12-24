79047ee50dff91f469b71af4b981800baedc9436
SamStar3 <samstar1044948@gmail.com>
2024-12-24 16:10:21 +0530

    file added

--- src/Tree/FoldableTree.java
+++ src/Tree/FoldableTree.java
@@ -1,2 +1,37 @@
-package Tree;public class FoldableTree {
+package Tree;
+
+public class FoldableTree {
+
+    boolean IsFoldable(Node node)
+    {
+        // your code
+        if (node == null) {
+            return true;
+        }
+        return isMirrorStructure(node.left, node.right);
+    }
+    boolean isMirrorStructure(Node left, Node right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return isMirrorStructure(left.left, right.right) &&
+                isMirrorStructure(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+
+        Node root1 = new Node(10);
+        root1.left = new Node(7);
+        root1.right = new Node(15);
+        root1.left.right = new Node(9);
+        root1.right.left = new Node(11);
+
+        FoldableTree tree1 = new FoldableTree();
+        System.out.println(tree1.IsFoldable(root1) ? "Yes" : "No");
+    }
 }
--- src/Tree/VerticalWidth.java
+++ src/Tree/VerticalWidth.java
@@ -1,2 +1,5 @@
-package Tree;public class VerticalWidth {
+package Tree;
+
+public class VerticalWidth {
+
 }
