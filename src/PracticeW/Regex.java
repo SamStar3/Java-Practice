7791f5ee669bf949f7bcfb5fedd7701bcf7b49d2
SamStar3 <samstar1044948@gmail.com>
2025-01-05 23:33:45 +0530

    file added

--- src/PracticeW/Regex.java
+++ src/PracticeW/Regex.java
@@ -1,2 +1,27 @@
-package PracticeW;public class Regex {
+package PracticeW;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
+
+//import static com.sun.tools.javac.main.Option.O;
+
+public class Regex {
+
+    public static void main(String[] args) {
+
+    String input =
+            "Hello World! ";
+    String pattern = "/b/w/b";
+            Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(input);
+    int count = 0;
+while(m.find())
+
+    {
+        count++;
+        System.out.println(count);
+    }
+}
 }
