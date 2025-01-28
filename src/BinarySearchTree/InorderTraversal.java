c7ecd10d9832eca92b8b2c983cd347aa981d071c
SamStar3 <samstar1044948@gmail.com>
2025-01-28 15:16:41 +0530

    file added

--- src/BinarySearchTree/InorderTraversal.java
+++ src/BinarySearchTree/InorderTraversal.java
@@ -1,8 +1,40 @@
 package BinarySearchTree;
 
+import java.util.ArrayList;
+
 public class InorderTraversal {
 
-    public static  void main(String[] args){
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        inOrderTraversal(root, result);
+        return result;
+    }
 
+    // Helper function to perform the in-order traversal.
+    void inOrderTraversal(Node root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        // Traverse the left subtree.
+        inOrderTraversal(root.left, result);
+        // Visit the current node.
+        result.add(root.data);
+        // Traverse the right subtree.
+        inOrderTraversal(root.right, result);
+    }
+    public static void main(String[] args) {
+
+        InorderTraversal tree = new InorderTraversal();
+
+        // Creating the BST.
+        Node root = new Node(5);
+        root.left = new Node(2);
+        root.right = new Node(7);
+        root.left.right = new Node(3);
+        root.right.right = new Node(8);
+
+        // Performing in-order traversal.
+        ArrayList<Integer> result = tree.inOrder(root);
+        System.out.println(result);
     }
 }
