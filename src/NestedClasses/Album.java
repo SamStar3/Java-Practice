4543ca145dbd3ea8777f1bb66ae8aeca0170391d
SamStar3 <samstar1044948@gmail.com>
2025-02-01 23:41:47 +0530

    file added

--- src/NestedClasses/Album.java
+++ src/NestedClasses/Album.java
@@ -1,4 +1,72 @@
 package NestedClasses;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Album {
+    private String name;
+    private String artist;
+    private SongList songList;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songList = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        return songList.add(new Song(title, duration));
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        Song song = songList.findSong(trackNumber);
+        if (song != null) {
+            playList.add(song);
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song song = songList.findSong(title);
+        if (song != null) {
+            playList.add(song);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+    public static class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<>();
+        }
+
+        private boolean add(Song song) {
+            if (findSong(song.getTitle()) == null) {
+                songs.add(song);
+                return true;
+            }
+            return false;
+        }
+
+        private Song findSong(String title) {
+            for (Song song : songs) {
+                if (song.getTitle().equalsIgnoreCase(title)) {
+                    return song;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            if (trackNumber > 0 && trackNumber <= songs.size()) {
+                return songs.get(trackNumber - 1);
+            }
+            return null;
+        }
+    }
 }
