d875d838c46e1d9042e932adaa97aab931df4e1d
SamStar3 <samstar1044948@gmail.com>
2025-02-14 00:12:37 +0530

    file added

--- src/BinarySearchTree/GetCount.java
+++ src/BinarySearchTree/GetCount.java
@@ -1,2 +1,24 @@
-package BinarySearchTree;public class GetCount {
+package BinarySearchTree;
+
+public class GetCount {
+    int getCount(Node root, int l, int h) {
+        // Base case: If the tree is empty
+        if (root == null) {
+            return 0;
+        }
+
+        // If the current node's value lies within the range
+        if (root.data >= l && root.data <= h) {
+            // Count this node and check in both left and right subtrees
+            return 1 + getCount(root.left, l, h) + getCount(root.right, l, h);
+        }
+
+        // If the current node's value is smaller than l, ignore the left subtree
+        if (root.data < l) {
+            return getCount(root.right, l, h);
+        }
+
+        // If the current node's value is greater than h, ignore the right subtree
+        return getCount(root.left, l, h);
+    }
 }
--- src/BinarySearchTree/LCA.java
+++ src/BinarySearchTree/LCA.java
@@ -1,2 +1,24 @@
-package BinarySearchTree;public class LCA {
+package BinarySearchTree;
+
+public class LCA {
+
+    Node LCA(Node root, Node n1, Node n2) {
+        // Base case: If the root is null, there is no LCA
+        if (root == null) {
+            return null;
+        }
+
+        // If both n1 and n2 are smaller than root, LCA lies in the left subtree
+        if (n1.data < root.data && n2.data < root.data) {
+            return LCA(root.left, n1, n2);
+        }
+
+        // If both n1 and n2 are greater than root, LCA lies in the right subtree
+        if (n1.data > root.data && n2.data > root.data) {
+            return LCA(root.right, n1, n2);
+        }
+
+        // If one node is on the left and the other is on the right (or one of them equals root), root is the LCA
+        return root;
+    }
 }
