ff8ded7b0b2a4a6f07813742139a938b9b124958
SamStar3 <samstar1044948@gmail.com>
2025-03-12 17:08:31 +0530

    file added

--- src/HackerRank/CompareTriplets.java
+++ src/HackerRank/CompareTriplets.java
@@ -1,2 +1,57 @@
-package HackerRank;public class CompareTriplets {
+package HackerRank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CompareTriplets {
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        int first =0;
+        int second = 0;
+
+        for(int i = 0; i < a.size(); i++){
+            if(a.get(i) > b.get(i)){
+                first++;
+            }else if(a.get(i) < b.get(i)){
+                second++;
+            }
+        }
+
+        return Arrays.asList(first,second);
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a,b); // Call method directly
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
