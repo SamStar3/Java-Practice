0bdf9f45710b455aa8d756a216b49bafd9912d5c
SamStar3 <samstar1044948@gmail.com>
2025-03-13 13:05:08 +0530

    file added

--- src/Interview/Salary.java
+++ src/Interview/Salary.java
@@ -1,2 +1,29 @@
-package Interview;public class Salary {
+package Interview;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Salary {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+//        sc.next();
+        Map<String,Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Give the " + i + " Person Name");
+            String key = sc.next();
+            System.out.println("Give the " + i + " Person Salary");
+            int value = sc.nextInt();
+            map.put(key, value);
+        }
+
+        System.out.println(map);
+
+
+    }
 }
