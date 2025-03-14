b4a9387bdea733b931ba0d18b8e0481665150138
SamStar3 <samstar1044948@gmail.com>
2025-03-14 20:44:45 +0530

    file added

--- src/Interview/Numbers.java
+++ src/Interview/Numbers.java
@@ -1,14 +1,16 @@
 package Interview;
 
 import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Numbers {
 
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
+//        int num = sc.nextInt();
 //        int arr[] = new int[num];
 //        for (int i = 0; i < num; i++){
 //            arr[i] = sc.nextInt();
@@ -26,8 +28,8 @@ public class Numbers {
 //            map.add(sc.nextInt());
 //        }
 
-        String numstr = String.valueOf(num);
-        Set<String> set = new HashSet<>();
+//        String numstr = String.valueOf(num);
+//        Set<String> set = new HashSet<>();
 
 //        solution(numstr,0,numstr.length() - 1,set);
 
@@ -44,8 +46,7 @@ public class Numbers {
 //            }
 //        }
 
-        System.out.println(numstr);
-    }
+//        System.out.println(numstr);
 
 //    static HashSet<String> solution(String numstr,int a, int z, Set<String> set){
 //
@@ -54,4 +55,35 @@ public class Numbers {
 //        }
 //
 //    }
+
+
+        int number = 123;
+        String numStr = String.valueOf(number);
+        Set<String> permutations = new HashSet<>();
+
+        int n = numStr.length();
+        char[] arr = numStr.toCharArray();
+
+        generatePermutations(arr, 0, n - 1, permutations);
+
+        for (String perm : permutations) {
+                System.out.println(perm);
+        }
+    }
+    private static void generatePermutations(char[] arr, int left, int right, Set<String> permutations) {
+        if (left == right) {
+            permutations.add(new String(arr));
+        } else {
+            for (int i = left; i <= right; i++) {
+                swap(arr, left, i);
+                generatePermutations(arr, left + 1, right, permutations);
+                swap(arr, left, i);
+            }
+        }
+    }
+    private static void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
