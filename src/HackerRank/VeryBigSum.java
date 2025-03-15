e86ed377f215c4c25dafe8233986e976c4128f3e
SamStar3 <samstar1044948@gmail.com>
2025-03-15 23:29:15 +0530

    file added

--- src/HackerRank/VeryBigSum.java
+++ src/HackerRank/VeryBigSum.java
@@ -1,2 +1,36 @@
-package HackerRank;public class VeryBigSum {
+package HackerRank;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class VeryBigSum {
+
+    public static long aVeryBigSum(List<Long> ar) {
+        long sum = 0;
+        for (long num : ar) {
+            sum += num;
+        }
+        return sum;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = VeryBigSum.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
