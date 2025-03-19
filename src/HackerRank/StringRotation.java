2ea7c3e6faba08f91dd9aecf0af05232a3da17e4
SamStar3 <samstar1044948@gmail.com>
2025-03-19 19:09:23 +0530

    file added

--- src/HackerRank/StringRotation.java
+++ src/HackerRank/StringRotation.java
@@ -1,2 +1,56 @@
-package HackerRank;public class StringRotation {
+package HackerRank;
+
+public class StringRotation {
+
+//    static String rotation(String s, String r, int n){
+//
+////        char[] chars = s.toCharArray();
+//        StringBuilder sb = new StringBuilder;
+//        char rotation = r.charAt(0);
+//        if (rotation == 'L'){
+//            for (int i = n - 1; i < s.length() - 1; i++){
+//                sb.append(s.charAt(i));
+//            }
+//        }
+//        String str = sb.toString();
+//        return str;
+//    }
+
+    static String leftRotation(String s, int n){
+
+        int len = s.length();
+        n %= len;
+
+        return s.substring(n) + s.substring(0,n);
+    }
+
+    static String rightRotation(String s, int n){
+
+        int len = s.length();
+        n%= len;
+
+        return s.substring(len - n) + s.substring(0, len - n);
+    }
+
+    public static void main(String[] args){
+
+        String s = "ZOHOCORPORATION";
+        String r = "L-ROTATION";
+        int n = 4;
+
+//        System.out.println(rotation(s,r,n));
+
+        String result;
+
+        if (r.equalsIgnoreCase("L-ROTATION")){
+            result = leftRotation(s,n);
+        }else if (r.equalsIgnoreCase("R-ROTATION")){
+            result = rightRotation(s,n);
+        }else {
+            System.out.println("Invalid Rotation type");
+            return;
+        }
+
+        System.out.println("Rotated String: " + result);
+    }
 }
