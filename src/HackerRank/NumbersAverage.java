a655fc6fbbb914b1661c1dbbf78ae0d924020cb2
SamStar3 <samstar1044948@gmail.com>
2025-04-04 21:30:48 +0530

    file added

--- src/HackerRank/NumbersAverage.java
+++ src/HackerRank/NumbersAverage.java
@@ -1,8 +1,10 @@
 package HackerRank;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 public class NumbersAverage {
 
--- src/Interview/MergeArrayLists.java
+++ src/Interview/MergeArrayLists.java
@@ -1,2 +1,37 @@
-package Interview;public class MergeArrayLists {
+package Interview;
+
+import java.util.ArrayList;
+
+public class MergeArrayLists {
+
+    public static void main(String[] args){
+
+        ArrayList<Integer> ls1 = new ArrayList<>();
+        ls1.add(10);
+        ls1.add(20);
+        ls1.add(30);
+        ls1.add(40);
+        ls1.add(50);
+        ls1.add(60);
+        ls1.add(70);
+        ls1.add(80);
+        ls1.add(90);
+        ls1.add(100);
+
+        ArrayList<Integer> ls2 = new ArrayList<>();
+        ls2.add(110);
+        ls2.add(120);
+        ls2.add(130);
+        ls2.add(140);
+        ls2.add(150);
+        ls2.add(160);
+        ls2.add(170);
+        ls2.add(180);
+        ls2.add(190);
+        ls2.add(200);
+
+        ls1.addAll(ls2);
+
+        System.out.println("Merged ArrayList: " + ls1);
+    }
 }
