75c1139370269c05cabf84c786a1109dad15e50d
SamStar3 <samstar1044948@gmail.com>
2025-04-09 20:37:37 +0530

    file added

--- src/Interview/Sum.java
+++ src/Interview/Sum.java
@@ -2,8 +2,19 @@ package Interview;
 
 public class Sum {
 
+
+    static void sum(int a, int b, int c){
+
+        System.out.println(a+b+c);
+    }
+
     public static void main(String[] args){
 
 
+        int a = 34;
+        int b =43;
+        int c = 42;
+
+        sum(a,b,c);
     }
 }
