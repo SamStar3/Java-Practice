56b86102a873dad37d295cc6bc1d73e17e01c244
SamStar3 <samstar1044948@gmail.com>
2025-04-09 20:43:43 +0530

    file added

--- src/Interview/Sample.java
+++ src/Interview/Sample.java
@@ -2,8 +2,13 @@ package Interview;
 
 public class Sample {
 
+    static void sample(String str){
+        System.out.println(str);
+    }
+
     public static void main(String[] args){
 
+        sample("sample program");
 
         System.out.println("Sample Program");
     }
