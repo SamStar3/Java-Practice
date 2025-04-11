26b33811ee140491988f503a78d49d16be34b5f0
SamStar3 <samstar1044948@gmail.com>
2025-04-11 23:41:32 +0530

    file added

--- src/Interview/MinOfMax.java
+++ src/Interview/MinOfMax.java
@@ -2,8 +2,17 @@ package Interview;
 
 public class MinOfMax {
 
+    static void minOfMax(int[] arr){
+
+
+    }
+
     public static void main(String[] args){
 
         System.out.println("MinOfMax");
+
+        int arr[] = {3,5,2,6,2};
+
+        minOfMax(arr);
     }
 }
