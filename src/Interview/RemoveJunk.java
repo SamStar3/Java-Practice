a850c140c5127b9496f71d6c6512fc98aecec03b
SamStar3 <samstar1044948@gmail.com>
2025-04-12 16:48:17 +0530

    file added

--- src/Interview/RemoveJunk.java
+++ src/Interview/RemoveJunk.java
@@ -1,2 +1,13 @@
-package Interview;public class RemoveJunk {
+package Interview;
+
+public class RemoveJunk {
+
+    public static void main(String[] args){
+
+
+        String str = "23442 Sam Pandian #$% 323";
+        str = str.replaceAll("[^a-zA-Z0-9]", "");
+
+        System.out.println(str);
+    }
 }
