14930966e14cd3a411c14e95d2679a9b621229a0
SamStar3 <samstar1044948@gmail.com>
2025-04-12 16:38:53 +0530

    file added

--- src/Interview/ReverseString.java
+++ src/Interview/ReverseString.java
@@ -1,2 +1,21 @@
-package Interview;public class ReverseString {
+package Interview;
+
+public class ReverseString {
+
+    public static void main(String[] args){
+
+        String str = "Selenium";
+
+        String rev = "";
+        int n = str.length();
+
+        for (int i = n-1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println(rev);
+
+        StringBuffer bf = new StringBuffer(str);
+        System.out.println(bf.reverse());
+    }
 }
