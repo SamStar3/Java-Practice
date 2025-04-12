5d1a9ad6bfaeb6465688ea30477e5e7a48cd52d8
SamStar3 <samstar1044948@gmail.com>
2025-04-12 20:01:30 +0530

    file added

--- src/Interview/ReverseInt.java
+++ src/Interview/ReverseInt.java
@@ -1,2 +1,22 @@
-package Interview;public class ReverseInt {
+package Interview;
+
+public class ReverseInt {
+
+    public static void main(String[] args){
+
+        int num = 435324;
+
+        int rev = 0;
+
+        while(num != 0){
+            rev = rev * 10 + num % 10;
+            num/=10;
+        }
+
+        System.out.println(rev);
+//
+//        StringBuffer str =new StringBuffer(String.valueOf(rev));
+
+        System.out.println(new StringBuffer(String.valueOf(rev)).reverse());
+    }
 }
