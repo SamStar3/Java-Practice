1dd8acc7bd52886911ee6a3be53f7dbb079159d8
SamStar3 <samstar1044948@gmail.com>
2025-04-16 00:23:28 +0530

    file added

--- src/Interview/MissingNumber.java
+++ src/Interview/MissingNumber.java
@@ -4,5 +4,19 @@ public class MissingNumber {
 
     public static void main(String[] args){
 
+        int arr[] = {1,2,3,4,5,6,8,9};
+        int sum = 0;
+        int totel = 0;
+
+        for (int i =0; i < arr.length; i++){
+            sum = sum+ arr[i];
+        }
+        System.out.println(sum);
+        for (int i = arr[0]; i <= arr[arr.length-1]; i ++){
+            totel =  totel + i;
+        }
+        System.out.println(totel);
+        System.out.println(totel - sum);
+
     }
 }
