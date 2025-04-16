fa53588560432b6a2b175b6d3e88abf26164f9e5
SamStar3 <samstar1044948@gmail.com>
2025-04-16 23:46:37 +0530

    file added

--- src/Interview/DuplicateElements.java
+++ src/Interview/DuplicateElements.java
@@ -1,2 +1,30 @@
-package Interview;public class DuplicateElements {
+package Interview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateElements {
+
+    public static void main(String[] args){
+
+        String[] names = {"Java", "JavaScript", "Ruby", "C", "Python", "Java", "C"};
+
+        for (int i = 0; i < names.length; i++){
+            for (int j = i + 1; j < names.length; j++){
+                if (names[i].equals(names[j])){
+                    System.out.println("DuplicateElements is : " + names[j]);
+
+                }
+            }
+        }
+
+        Set<String> store = new HashSet<>();
+
+        for (String name : names){
+//            store.add(name);
+            if (store.add(name) == false){
+                System.out.println(name);
+            }
+        }
+    }
 }
