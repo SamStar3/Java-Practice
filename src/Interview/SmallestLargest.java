8eef7d383e5b7baa377bdc65d8f0625188deefc9
SamStar3 <samstar1044948@gmail.com>
2025-04-16 23:53:52 +0530

    file added

--- src/Interview/SmallestLargest.java
+++ src/Interview/SmallestLargest.java
@@ -1,2 +1,22 @@
-package Interview;public class SmallestLargest {
+package Interview;
+
+public class SmallestLargest {
+
+    public static void main(String[] args){
+
+        int[] numbers = {4,5235,5353,3,3,12,9};
+        int largest = numbers[0];
+        int smallest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++){
+            if (numbers[i] > largest){
+                largest = numbers[i];
+            }else if (numbers[i] < smallest){
+                smallest = numbers[i];
+            }
+        }
+
+        System.out.println("Largest Number is : " + largest);
+        System.out.println("Smallest Number is : " + smallest);
+    }
 }
