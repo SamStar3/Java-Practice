791315f5c9e933255a33c9eaca0ad1b8bdd87576
SamStar3 <samstar1044948@gmail.com>
2025-04-17 12:32:56 +0530

    file added

---
+++ src/Interview/CountDigit.java
@@ -0,0 +1,13 @@
+package Interview;
+
+public class CountDigit {
+
+    public static void main(String[] args){
+
+        int[] arr = {2,3,4,6,2,3};
+        String str = "Iam Here";
+
+        System.out.println(arr.length);
+        System.out.println(str.length());
+    }
+}
