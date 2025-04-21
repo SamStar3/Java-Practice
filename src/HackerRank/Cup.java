a865c12f3b4741381c1f7de29863e2a61fa0066d
SamStar3 <samstar1044948@gmail.com>
2025-04-21 21:40:09 +0530

    file added

--- src/HackerRank/Cup.java
+++ src/HackerRank/Cup.java
@@ -4,5 +4,12 @@ public class Cup {
 
     public static void main(String[] args){
 
+        int s = 94;
+        int d = 42;
+        int a = 2;
+        int result = 0;
+
+        System.out.println(result);
+
     }
 }
