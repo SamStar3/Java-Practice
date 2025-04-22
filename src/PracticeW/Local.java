77236b4fa9a8c295e6ca264328aaba0c51f332e2
SamStar3 <samstar1044948@gmail.com>
2025-04-22 22:02:53 +0530

    file added

--- src/PracticeW/Local.java
+++ src/PracticeW/Local.java
@@ -4,5 +4,14 @@ public class Local {
 
     public static void main(String[] args){
 
+        System.out.println("Local");
+
+        int l = 90;
+        int o = 98;
+        int c = 56;
+        int al = 67;
+
+        System.out.println((l+o+c+al)/4);
+
     }
 }
