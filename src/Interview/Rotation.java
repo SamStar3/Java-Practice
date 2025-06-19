39cf8f87a0cbe27876509e94720116d7d680cefb
SamStar3 <samstar1044948@gmail.com>
2025-06-19 20:10:13 +0530

    file added

--- src/Interview/Rotation.java
+++ src/Interview/Rotation.java
@@ -1,2 +1,40 @@
-package Interview;public class Rotation {
+package Interview;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+public class Rotation {
+
+    public static void main(String[] args){
+
+        String s = "ZohoCorporation";
+        String r ="R-Rotation";
+        int n = 4;
+
+
+
+
+        int len = s.length();
+        n = n % len;
+        String res = "";
+
+        if (r.equals("L-Rotation")){
+            for (int i = n; i < s.length(); i++){
+                res += s.charAt(i);
+            }
+            for (int i = 0; i < n; i++){
+                res+= s.charAt(i);
+            }
+        }else {
+            for (int i = len - n; i < len; i ++){
+                res+= s.charAt(i);
+            }
+            for (int i = 0; i < len - n; i++){
+                res += s.charAt(i);
+            }
+        }
+
+        System.out.println(res);
+//        s= s+'w';
+        System.out.println(s);
+    }
 }
