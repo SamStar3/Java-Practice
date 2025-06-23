b21fd3fa0570b6ac54ea6f5e666499d1eceafa46
SamStar3 <samstar1044948@gmail.com>
2025-06-23 23:04:41 +0530

    file added

--- src/Interview/ArrayDigit.java
+++ src/Interview/ArrayDigit.java
@@ -1,2 +1,26 @@
-package Interview;public class ArrayDigit {
+package Interview;
+
+public class ArrayDigit {
+
+    public static void main(String[] args){
+
+        int arr[] = {8,2,1,1,9};
+
+        int count[] = new int[10];
+        for (int i : arr){
+            count[i]++;
+        }
+
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 9; i >=0 ; i--){
+
+            while (count[i]> 0){
+                str.append(i);
+                count[i]--;
+            }
+        }
+
+        System.out.println(str);
+    }
 }
