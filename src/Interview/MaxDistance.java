7879e23488b015ef8a01702f72e81e87ad1992d8
SamStar3 <samstar1044948@gmail.com>
2025-06-23 22:40:21 +0530

    file added

--- src/Interview/MaxDistance.java
+++ src/Interview/MaxDistance.java
@@ -1,4 +1,39 @@
 package Interview;
 
 public class MaxDistance {
+
+    public static void main(String[] args){
+
+        String str = "abcbda";
+        int maxDis = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            int first = -1;
+
+            for (int j = 0; j < str.length(); j++){
+                if (str.charAt(j)==ch){
+                    first = j;
+                    break;
+                }
+            }
+            int last = -1;
+
+            for (int j = str.length()-1; j >= 0; j--){
+
+                if (str.charAt(j) == ch){
+                    last = j;
+                    break;
+                }
+            }
+
+            int dis = last - first;
+
+            if (dis > maxDis){
+                maxDis = dis;
+            }
+        }
+
+        System.out.println(maxDis - 1   );
+    }
 }
