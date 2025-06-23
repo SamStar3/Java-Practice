3b5b6b56977ae95abbb61e001566edbbeac6ad7d
SamStar3 <samstar1044948@gmail.com>
2025-06-23 22:50:21 +0530

    file added

--- src/Interview/Pattern.java
+++ src/Interview/Pattern.java
@@ -1,2 +1,22 @@
-package Interview;public class Pattern {
+package Interview;
+
+public class Pattern {
+
+    public static void main(String[] args){
+
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++){
+            int t = i;
+
+            for (int j = 1; j <= i ; j++){
+                System.out.print(t + " ");
+
+                t+=  n - j;
+            }
+            System.out.println();
+        }
+
+    }
 }
